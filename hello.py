45549fef025accf44a186fb2e9af9180c53888af
Amanda Filizola <apf@cesar.org.br>
2019-07-24 13:25:29 -0300

    primeiro commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+print ("Hello World")
+
+numero = input("Que numero?  ")
+txt = "o numero escolhido foi {}"
+print(txt.format(numero))
+
+
---
+++ meusalario.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+salarioPorHora = input("Quanto você ganha por hora?  ")
+horasTrabalhadas = input("Quantas horas você trabalha por mês?  ")
+
+salarioBruto = salarioPorHora*horasTrabalhadas;
+
+emIR = salarioBruto*0.11
+emINSS = salarioBruto*0.08
+emSindicato = salarioBruto*0.05
+
+salarioLiquido = salarioBruto - emIR - emINSS - emSindicato
+
+txtBruto = "Salário Bruto: R${}"
+print(txtBruto.format(salarioBruto))
+
+txtIR = "IR (11%): R$ {}"
+print(txtIR.format(emIR))
+
+txtINSS = "INSS (8%): R$ {}"
+print(txtINSS.format(emINSS))
+
+txtSindicato = "Sindicato (5%): R$ {}"
+print(txtSindicato.format(emSindicato))
+
+liquido = "Salário Líquido: R$ {}"
+print(liquido.format(salarioLiquido))
