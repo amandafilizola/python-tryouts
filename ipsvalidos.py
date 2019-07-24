be905e213393e135ab3b42285f05aa505a6a2916
Amanda Filizola <apf@cesar.org.br>
2019-07-24 15:56:05 -0300

    Checando se ips são válidos ou não: manipulação de arquivos

---
+++ ipsvalidos.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#coding: utf-8
+validos = []
+invalidos = []
+
+#ping
+def ping(ip):
+    import os, platform
+
+    if  platform.system().lower()=="windows":
+        ping_str = "-n 1"
+    else:
+        ping_str = "-c 1"
+
+    resposta = os.system("ping " + ping_str + " " + ip)
+    return resposta == 0
+
+#abrindo ip
+with open('ips.txt', 'r') as f:
+    content = f.readlines()
+
+#lendo cada ip que vem do txt
+for each in content:
+    each = each.replace('\n','')
+    print(each)
+    if(ping(each)):
+        validos.append(each)
+    else:
+        invalidos.append(each)
+
+
+#printando output
+with open('output.txt','w') as f:
+    f.write("[Endereços Válidos]\n")
+    for element in validos:
+        f.write(element)
+        f.write("\n")
+    f.write("\n[Endereços Inválidos]\n")
+    for element in invalidos:
+        f.write(element)
+        f.write("\n")
+
+
+
+
+print("\n[Endereços Válidos]")
+for element in validos:
+    print(element)
+
+print("\n[Endereços Inválidos]")
+for element in invalidos:
+    print(element)
