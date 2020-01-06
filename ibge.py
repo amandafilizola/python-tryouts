157c5674e8dcb405a03f76c2509e5f9734f9c9b0
Amanda Filizola <apf@cesar.org.br>
2020-01-06 13:58:01 -0300

    exercicio ibge

---
+++ ibge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import csv
+
+with open('RedeUrbana.csv', 'r') as dados:
+    with open('saida.txt','w') as saida:
+        reader = csv.reader(dados)
+        next(reader, None)
+        for registro in reader:
+            saida.write(f'{registro[3]}, {registro[4]}, {registro[5]}\n')
+
