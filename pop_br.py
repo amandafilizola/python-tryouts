99c1d83365e844d1c8339c87eb18be27f7f25728
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-02-19 08:21:26 -0300

    Atualizando gráfico de população

--- pop_br.py
+++ pop_br.py
@@ -1,6 +1,21 @@
+import matplotlib.pyplot as plt
 
-dados = open('populacao_brasileira.csv').readlines()
+dados = open('populacao-brasileira.csv').readlines()
 
 x = []
 y = []
 
+for i in range(len(dados)):
+  if i != 0:
+    linha  = dados[i].split(";")
+    x.append(int(linha[0]))
+    y.append(int(linha[1]))
+
+anomin = min(x)
+anomax = max(x)
+
+plt.bar(x,y)
+plt.title('Crescimento da populaão brasileira '+ str(anomin) + '-' + str(anomax))
+plt.xlabel('Ano')
+plt.ylabel('População em milhões')
+plt.show()
